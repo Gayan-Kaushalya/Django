4187f13a4812d75c50568f7e609e65ba794c161d
Gayan Kaushalya <gaykaush@gmail.com>
2024-02-08 10:57:08 +0530

    Saved Data to a Database With a Model and a Migration

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,8 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from listings.models import Band
+
 def hello(request):
-    return HttpResponse('<h1>Hello, World!</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello, World!</h1>
+        <p>Here are the bands:</p>
+        <ul>
+            {"".join([f'<li>{band.name}</li>' for band in bands])}
+        </ul>
+    """)
 
 def about(request):
     return HttpResponse('<h1>About Us</h1> <p>We love to sell things!</p>')
--- merchex/merchex/settings.py
+++ merchex/merchex/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'listings',                          # So this is the last to load
+    
 ]
 
 MIDDLEWARE = [
