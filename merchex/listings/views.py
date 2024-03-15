19addec144977df505f8b1fe48288cdf85f325bf
Gayan Kaushalya <gaykaush@gmail.com>
2024-03-15 20:32:44 +0530

    Separate App Logic From Presentation With a Django Template

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,6 +5,12 @@ from listings.models import Band
 
 def hello(request):
     bands = Band.objects.all()
+    return render(request, 
+                  'listings/hello.html', 
+                  {'bands':bands})  #context dictionary
+    
+    
+    ''' I used this before 2024.03.15
     return HttpResponse(f"""
         <h1>Hello, World!</h1>
         <p>Here are the bands:</p>
@@ -12,12 +18,35 @@ def hello(request):
             {"".join([f'<li>{band.name}</li>' for band in bands])}
         </ul>
     """)
+    Under the hood, the  render  function creates an HttpResponse object with the HTML from our template and returns it. So our view is still returning an HttpResponse (which it must do to be a view).
+    '''
+    
+''' Anti Pattern
+
+def hello(request):
+    bands = Band.objects.all()
+    return HttpResponse(f”””
+        <html>
+            <head><title>Merchex</title><head>
+            <body>
+                <h1>Hello Django!</h1>
+                <p>My favourite bands are:<p>
+                <ul>
+                    <li>{bands[0].name}</li>
+                    <li>{bands[1].name}</li>
+                    <li>{bands[2].name}</li>
+                </ul>
+            </body>
+        </html>
+    ”””)
+'''
+
 
 def about(request):
-    return HttpResponse('<h1>About Us</h1> <p>We love to sell things!</p>')
+    return render(request, 'listings/about.html')
 
 def listings(request):
-    return HttpResponse('<h1>Listings</h1> <p>Check out our listings!</p>')
+    return render(request, 'listings/listings.html')
 
 def contact(request):
-    return HttpResponse('<h1>Contact Us</h1> <p>Get in touch with us!</p>')
+    return render(request, 'listings/contact.html')
